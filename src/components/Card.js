b4614f754e47129e11bf0c056c6848910187d885
Gurvinder Dhillon <108829574+gurvinder-dhillon@users.noreply.github.com>
2022-12-23 17:44:51 -0800

    fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ function Card(props) {
                     <h3 className="mb-0">{props.blog.title}</h3>
                     <div className="mb-1 text-muted">{props.blog.date}</div>
                     <p className="card-text mb-auto">{props.blog.desc}</p>
-                    <a href="#" className="stretched-link">Continue reading</a>
+                    <a href="/promo" className="stretched-link">Continue reading</a>
                 </div>
                 <div className="col-auto d-none d-lg-block">
                     <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>

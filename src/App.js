9517d6c76bce5712b39586942acccad477472bb6
Gurvinder Dhillon <108829574+gurvinder-dhillon@users.noreply.github.com>
2023-04-11 09:25:35 -0700

    test commit 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,33 @@ import { Nav } from './components/Nav';
 import CardContainer from './components/CardContainer.js';
 import Promo from './components/Promo';
 import Calc from './components/Calc';
+import DessertList from './components/DessertList'
+import Test from './components/Test';
+import ToDo from './components/ToDo';
+import Formr from './components/Formr';
+
+const desserts = [
+  {
+    name: "Chocolate Cake",
+    calories: 400,
+    createdAt: "2022-09-01",
+  },
+  {
+    name: "Ice Cream",
+    calories: 200,
+    createdAt: "2022-01-02",
+  },
+  {
+    name: "Tiramisu",
+    calories: 300,
+    createdAt: "2021-10-03",
+  },
+  {
+    name: "Cheesecake",
+    calories: 600,
+    createdAt: "2022-01-04",
+  },
+];
 
 function Heading(props) {
   console.log(props);
@@ -12,6 +39,8 @@ function Heading(props) {
     < h1 className="App-header" >{props.head}</h1 >
   );
 }
+
+
 
 function App() {
   return (
@@ -23,6 +52,10 @@ function App() {
         <Route path="/" element={<CardContainer />} />
         <Route path="/promo" element={<Promo />} />
         <Route path="/calc" element={<Calc />} />
+        <Route path="/desserts" element={<DessertList data={desserts} />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/todo" element={<ToDo />} />
+        <Route path="/formr" element={<Formr />} />
       </Routes>
     </div>
   );
